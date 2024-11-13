357c8ad8d63e3d16befb5d33db8e4aa8af2798fb
bavanmaganti <113656667+bavanmaganti@users.noreply.github.com>
2024-11-13 21:55:38 +0000

    added api and dynamic route

--- app.py
+++ app.py
@@ -1,10 +1,35 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,jsonify
 
 app=Flask(__name__)
 
+Events=[
+  {
+    'id':1,
+    'Eventname':'coding',
+    'Date':'12/12/2024',
+    'time':'10am-2pm'
+  },
+  {
+    'id':2,
+    'Eventname':'Music',
+    'Date':'12/12/2024',
+    'time':'10am-2pm'
+  },
+  {
+    'id':3,
+    'Eventname':'Dance',
+    'Date':'12/12/2024',
+    'time':'10am-2pm'
+  }
+]
+
 @app.route("/")
 def hello_world():
-  return render_template('home.html')
+  return render_template('home.html',event=Events)
+
+@app.route('/api/Events')
+def list_Events():
+  return jsonify(Events)
 
 if __name__ =='__main__':
   app.run(host='0.0.0.0',debug=True)
